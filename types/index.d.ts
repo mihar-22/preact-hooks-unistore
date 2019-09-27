e22511fdf66cd41fa914384b069f698991621dc0
Rahim Alwer <rahim_alwer@hotmail.com>
2019-09-27 10:51:58 +1000

    fix(types): add context and provider types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,6 +4,26 @@ import {
   Store
 } from 'unistore'
 
+import {ComponentChildren, FunctionComponent} from "preact";
+
+interface Consumer<T> extends FunctionComponent<{
+  children: (value: T) => ComponentChildren
+}> {}
+
+interface Provider<T> extends FunctionComponent<{
+  value: T,
+  children: ComponentChildren
+}> {}
+
+interface Context<T> {
+  Consumer: Consumer<T>;
+  Provider: Provider<T>;
+}
+
+export interface StoreContext<K> extends Context<Store<K>> {}
+
+export interface StoreProvider<K> extends Provider<Store<K>> {}
+
 export function shallowEqual(left: any, right: any): boolean;
 
 export function useSelector<TState, TSelected>(
